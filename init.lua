bb82a19849f8203ac47488c0726ff357b63eaae3
kemal <kemalyldrim@gmail.com>
2024-03-06 15:45:52 +0000

    kickstart initialized

--- init.lua
+++ init.lua
@@ -1,49 +1,39 @@
-vim.loader.enable()
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+--[[
 
-if not vim.loop.fs_stat(lazypath) then
-	vim.fn.system({
-		"git",
-		"clone",
-		"--filter=blob:none",
-		"https://github.com/folke/lazy.nvim.git",
-		"--branch=stable", -- latest stable release
-		lazypath,
-	})
-end
+=====================================================================
+==================== READ THIS BEFORE CONTINUING ====================
+=====================================================================
+========                                    .-----.          ========
+========         .----------------------.   | === |          ========
+========         |.-""""""""""""""""""-.|   |-----|          ========
+========         ||                    ||   | === |          ========
+========         ||       NVIM         ||   |-----|          ========
+========         ||                    ||   | === |          ========
+========         ||                    ||   |-----|          ========
+========         ||:Tutor              ||   |:::::|          ========
+========         |'-..................-'|   |____o|          ========
+========         `"")----------------(""`   ___________      ========
+========        /::::::::::|  |::::::::::\  \ no mouse \     ========
+========       /:::========|  |==hjkl==:::\  \ required \    ========
+========      '""""""""""""'  '""""""""""""'  '""""""""""'   ========
+========                                                     ========
+=====================================================================
+=====================================================================
 
-vim.opt.rtp:prepend(lazypath)
+-- Set <space> as the leader key
+-- See `:help mapleader`
+--  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
 
-require("config.options")
-require("config.autocmds")
-require("config.keymap")
+-- [[ Setting options ]]
+require 'kemal/options'
 
+-- [[ Basic Keymaps ]]
+require 'kemal/keymaps'
 
-require("lazy").setup({
+-- [[ Install `lazy.nvim` plugin manager ]]
+require 'kemal/lazy-bootstrap'
 
-	spec = {
-		{ import = "plugins" },
-	},
-	install = { colorscheme = { "nightfox", "habamax" } },
-	change_detection = {
-		notify = false
-	},
-	performance = {
-		rtp = {
-			-- disable some rtp plugins
-			disabled_plugins = {
-				"gzip",
-				-- "matchit",
-				-- "matchparen",
-				"man",
-				"rplugin",
-				"netrwPlugin",
-				"tarPlugin",
-				"tohtml",
-				"tutor",
-				"zipPlugin",
-			},
-		},
-	}
-})
-
+-- The line beneath this is called `modeline`. See `:help modeline`
+-- vim: ts=2 sts=2 sw=2 et
---
+++ lua/kemal/lazy-bootstrap.lua
@@ -0,0 +1,25 @@
+-- [[ Install `lazy.nvim` plugin manager ]]
+--    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
+local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
+if not vim.loop.fs_stat(lazypath) then
+  local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
+  vim.fn.system { 'git', 'clone', '--filter=blob:none', '--branch=stable', lazyrepo, lazypath }
+end ---@diagnostic disable-next-line: undefined-field
+vim.opt.rtp:prepend(lazypath)
+
+
+-- [[ Configure and install plugins ]]
+require('lazy').setup {
+  -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
+  'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
+
+  -- "gc" to comment visual regions/lines
+  { 'numToStr/Comment.nvim', opts = {} },
+
+  -- modular approach: using `require 'path/name'` will
+  -- include a plugin definition from file lua/path/name.lua
+
+  { import = 'kickstart.plugins' },
+  { import = 'kemal.plugins' },
+}
+-- vim: ts=2 sts=2 sw=2 et
