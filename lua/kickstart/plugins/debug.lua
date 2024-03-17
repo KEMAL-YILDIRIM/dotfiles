97f04e51ec63c3675ed66fdefd195e5334ef4271
kemal <kemalyldrim@gmail.com>
2024-03-17 23:04:35 +0000

    debug fixed

--- lua/kickstart/plugins/debug.lua
+++ lua/kickstart/plugins/debug.lua
@@ -28,14 +28,22 @@ return {
         args = {'--interpreter=vscode'}
       }
       
-      vim.g.dotnet_build_project = function()
-        local default_path = vim.fn.getcwd() .. '/'
+      local getProjPath = function()
+        local default_path = vim.fn.expand('%:p') .. '\\'
+
         if vim.g['dotnet_last_proj_path'] ~= nil then
-            default_path = vim.g['dotnet_last_proj_path']
+          default_path = vim.g['dotnet_last_proj_path']
         end
-        local path = vim.fn.input('Path to your *proj file', default_path, 'file')
-        vim.g['dotnet_last_proj_path'] = path
-        local cmd = 'dotnet build -c Debug ' .. path .. ' > /dev/null'
+        
+        vim.g['dotnet_last_proj_path'] = vim.fn.input('Input your *cs.proj folder path | ', default_path, 'file')
+        return vim.g['dotnet_last_proj_path']
+      end
+
+      vim.g.dotnet_build_project = function()
+       
+        local path = getProjPath()
+        -- local cmd = 'dotnet build -c Debug ' .. path .. ' > /dev/null'
+        local cmd = 'dotnet build -c Debug ' .. path
         print('')
         print('Cmd to execute: ' .. cmd)
         local f = os.execute(cmd)
@@ -47,33 +55,45 @@ return {
       end
     
       vim.g.dotnet_get_dll_path = function()
-        local request = function()
-            return vim.fn.input('Path to dll', vim.fn.getcwd() .. '/bin/Debug/', 'file')
+        
+        if vim.g['dotnet_last_proj_path'] == nil then
+          print('\nroot cs.proj path missing')
+          vim.g['dotnet_last_proj_path'] = getProjPath()
         end
-    
+        
+        local request = function()
+          return vim.fn.input('Input your project dll file path | ', vim.g['dotnet_last_proj_path'] .. 'bin\\Debug\\', 'file')
+        end
+        
         if vim.g['dotnet_last_dll_path'] == nil then
             vim.g['dotnet_last_dll_path'] = request()
         else
-            if vim.fn.confirm('Do you want to change the path to dll?\n' .. vim.g['dotnet_last_dll_path'], '&yes\n&no', 2) == 1 then
-                vim.g['dotnet_last_dll_path'] = request()
-            end
+          if vim.fn.confirm('Would you like to change your dll file path?\n' .. vim.g['dotnet_last_dll_path'], '&yes\n&no', 2) == 1 then
+            vim.g['dotnet_last_dll_path'] = request()
+          end
         end
     
         return vim.g['dotnet_last_dll_path']
       end
     
       local config = {
-          {
-            type = "coreclr",
-            name = "launch - netcoredbg",
-            request = "launch",
-            program = function()
-                if vim.fn.confirm('Should I recompile first?', '&yes\n&no', 2) == 1 then
-                    vim.g.dotnet_build_project()
-                end
-                return vim.g.dotnet_get_dll_path()
-            end,
+        {
+          type = "coreclr",
+          name = "launch - netcoredbg",
+          request = "launch",
+          env = "ASPNETCORE_ENVIRONMENT=QA",
+          args = {
+            "/p:EnvironmentName=QA", -- this is a msbuild jk
+            "--urls=http://localhost:5180",
+            "--environment=QA",
           },
+          program = function()
+            if vim.fn.confirm('Should I recompile first?', '&yes\n&no', 2) == 1 then
+              vim.g.dotnet_build_project()
+            end
+            return vim.g.dotnet_get_dll_path()
+          end,
+        },
       }
       dap.configurations.cs = config
 
