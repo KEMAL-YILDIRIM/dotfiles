e33ddd8df3e7d9fd1afdb162d575484047d0e125
Kemal Yildirim <kemalyldrim@gmail.com>
2025-03-12 11:05:35 +0000

    database configuration updated

--- lua/plugins/database.lua
+++ lua/plugins/database.lua
@@ -1,7 +1,7 @@
 return {
   'kristijanhusak/vim-dadbod-ui',
   dependencies = {
-    { 'tpope/vim-dadbod', lazy = true },
+    { 'tpope/vim-dadbod',                     lazy = true },
     { 'kristijanhusak/vim-dadbod-completion', ft = { 'sql', 'mysql', 'plsql' }, lazy = true }, -- Optional
   },
   cmd = {
@@ -11,7 +11,19 @@ return {
     'DBUIFindBuffer',
   },
   init = function()
-    -- Your DBUI configuration
     vim.g.db_ui_use_nerd_fonts = 1
+    -- Your DBUI configuration
+    vim.g.db_ui_winwidth = 40
+    vim.g.db_ui_use_nvim_notify = 1
+    vim.g.db_ui_icons = {
+      expanded = '',
+      collapsed = '',
+      saved_query = '',
+      new_query = '󰎔',
+      tables = '󰓫',
+      buffers = '',
+      connection_ok = '✓',
+      connection_error = '✕',
+    }
   end,
 }
