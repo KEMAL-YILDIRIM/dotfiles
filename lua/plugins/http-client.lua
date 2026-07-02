b51669d0cc5f968c4bcb6384236f9fbac1a2fe88
Kemal YILDIRIM <kemalyldrim@gmail.com>
2026-07-02 12:25:00 +0100

    🔧 chore: enable kulala global keymaps under <leader>r

--- lua/plugins/http-client.lua
+++ lua/plugins/http-client.lua
@@ -5,15 +5,11 @@ return {
   },
   {
     'mistweaverco/kulala.nvim',
-    keys = {
-      { '<leader>rs', desc = 'Send [R]equest' },
-      { '<leader>ra', desc = 'Send [A]ll [R]equests' },
-      { '<leader>ro', desc = '[O]pen [R]equest scratchpad' },
-    },
     ft = { 'http', 'rest' },
     opts = {
-      -- your configuration comes here
-      global_keymaps = false,
+      global_keymaps = true,
+      global_keymaps_prefix = '<leader>r',
+      ft = { 'http', 'rest' },
     },
   },
 }
